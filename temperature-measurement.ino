e61b201199b115d812451a61b73e2dd5524006a9
Nelson Silva <nelsonsilvadev@gmail.com>
2021-05-28 00:26:43 +0100

    Add Temperature Measurement and Tilt Switch

---
+++ temperature-measurement.ino
@@ -0,0 +1,12 @@
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	float sensorValue = analogRead(A0);
+	float temperature = (sensorValue / 1023) * 5 * 100;
+
+	Serial.print("The temperature degree is: ");
+	Serial.print(temperature);
+	delay(1000);
+}
